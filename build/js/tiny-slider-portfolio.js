007012ed0e7401d5d5d45e569c4515a4a09e497c
IlonaArt <ilonasurdream@gmail.com>
2018-12-25 23:50:10 +0300

    Added new slider

---
+++ build/js/tiny-slider-portfolio.js
@@ -0,0 +1,1 @@
+tns({container:"#portfolio-slider",items:1,controlsContainer:"#customize-controls",navContainer:"#customize-thumbnails",navAsThumbnails:!0});
---
+++ src/js/tiny-slider-portfolio.js
@@ -0,0 +1,7 @@
+var slider = tns({
+"container": "#portfolio-slider",
+"items": 1,
+"controlsContainer": "#customize-controls",
+"navContainer": "#customize-thumbnails",
+"navAsThumbnails": true
+});
