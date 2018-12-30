e9a93d6de38f532bb669429cf8c4a27eb13f423b
IlonaArt <ilonasurdream@gmail.com>
2018-12-30 19:27:58 +0300

    Added some changes in slider

--- src/js/tiny-slider-portfolio.js
+++ src/js/tiny-slider-portfolio.js
@@ -2,6 +2,6 @@ var slider = tns({
 "container": "#portfolio-slider",
 "items": 1,
 "controlsContainer": "#customize-controls",
-"navContainer": "#customize-thumbnails",
+/*"navContainer": "#customize-thumbnails",*/
 "navAsThumbnails": true
 });
